acd43dc6f4c09de28b60c9eab7180d59e2cefd88
Grind3189 <bellenlowell@gmail.com>
2025-10-26 00:15:19 +0800

    2015/d1/p2

--- 2015/d1/main.py
+++ 2015/d1/main.py
@@ -1,17 +1,41 @@
 # --- Day 1: Not Quite Lisp ---
 
+
+def p1(input):
+    floor = 0
+    for char in content:
+        if char == "(":
+            floor += 1
+        elif char == ")":
+            floor -= 1
+
+    print(f"Final floor: {floor}")
+
+
+def p2(input):
+    floor = 0
+    position = 0
+
+    for i, char in enumerate(content):
+        if char == "(":
+            floor += 1
+        elif char == ")":
+            floor -= 1
+
+        if floor == -1:
+            position = i
+            break
+
+    print(f"First character position that causes santa to basement: {position}")
+
+
 try:
     with open("input.txt", "r") as file:
         content = file.read().strip()
 
-        floor = 0
-        for char in content:
-            if char == "(":
-                floor += 1
-            elif char == ")":
-                floor -= 1
+        p1(content)
+        p2(content)
 
-        print(f"Floor: {floor}")
 except FileNotFoundError:
     print("File not found")
 
